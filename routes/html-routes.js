59fa55e6149532d5f16ae82cede26570b408589b
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2019-11-26 19:26:31 -0500

    add recipe url

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -28,8 +28,12 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
   
-  app.get("/search", function(req,res) {
+  app.get("/search", isAuthenticated, function(req,res) {
     res.sendFile(path.join(__dirname, "../public/search.html"));
+  });
+
+  app.get("/recipe", isAuthenticated, function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/recipedetails.html"));
   });
 
 };
